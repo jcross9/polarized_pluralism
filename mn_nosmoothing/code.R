b700de061eedbacc9af70942b0cd6a23c0ac84b3
Alexander Furnas <alexanderfurnas@Zander-Computer.attlocal.net>
2020-01-23 15:31:40 -0600

    added vector pseudocode

--- mn_nosmoothing/code.R
+++ mn_nosmoothing/code.R
@@ -3,6 +3,12 @@ testing <- ifelse(Sys.getenv("TESTING") == TRUE, TRUE, FALSE)
 source("lib/mn_model/runsetup.R")
 x       <- getCounts(RSnosmooth)
 
-runMN(U = U, varInt = RSnosmooth, varIntfake = RSfake_nosmooth, sfun = sfun_nosmooth, counts = x, 
+data <- read_csv("TBD")
+
+Major_subsets <- data %>% group_by(Major) %>% group_split() 
+
+estimates <- lapply(Major_subsets, function(i) 
+runMN(U = i, varInt = RSnosmooth, varIntfake = RSfake_nosmooth, sfun = sfun_nosmooth, counts = x, 
       IC = c("bic", "noc"), mu = mu, testing = testing, fixedcost = 1e-5,
       measures = c("expected_posterior", "real_posterior"))
+)
