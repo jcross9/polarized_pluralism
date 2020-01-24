494db0d7eec18094bb7b9db7cf3ec2b12d6584c5
Alexander Furnas <alexanderfurnas@Zander-Computer.local>
2020-01-24 12:11:53 -0600

    create vocab

--- source/analysis/mn_nosmoothing/code.R
+++ source/analysis/mn_nosmoothing/code.R
@@ -7,8 +7,15 @@ data <- read_csv("TBD")
 
 Major_subsets <- data %>% group_by(Major) %>% group_split() 
 
-estimates <- lapply(Major_subsets, function(i) 
-runMN(U = i, varInt = RSnosmooth, varIntfake = RSfake_nosmooth, sfun = sfun_nosmooth, counts = x, 
+preprocess <- lapply(Major_subsets, function(i) 'STUFF HAPPENS HERE TO MAKE COUNTS TK')
+
+vocab <- readRDS("/Users/alexanderfurnas/Downloads/repo/data/vocab.rds")
+
+vocab_split <- vocab %>% lapply(function(i) str_split(i, " ", simplify = T))
+
+vocab_split <- vocab_split %>% unlist()
+
+runMN(U = U, varInt = i$party, varIntfake = RSfake_nosmooth, sfun = sfun_nosmooth, counts = i$count, 
       IC = c("bic", "noc"), mu = mu, testing = testing, fixedcost = 1e-5,
       measures = c("expected_posterior", "real_posterior"))
-)
+
